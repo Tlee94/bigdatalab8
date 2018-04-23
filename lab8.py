e1a532b1b98a3ac350451f765314a374062851c8
Tim <tlee9400@gmail.com>
2018-04-23 19:03:27 -0400

    update lab8

--- lab8.py
+++ lab8.py
@@ -1,17 +1,24 @@
-#Timothy Lee
+    # Timothy Lee
+import sys
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
+def main(sc):
+    rdd = sc.parallelize(range(1000), 10)
+    print (rdd.mean())
+
 if __name__ == "__main__":
+    #reload(sys)
+    #sys.setdefaultencoding('utf8')
     sc = SparkContext()
     # Execute Main functionality
     df = SparkSession(sc)
     df = df.read.load('nyc_restaurants.csv',
-                     format='csv',
-                     header=True,
-                     inferSchema=True)
-
+                      format='csv',
+                      header=True,
+                      inferSchema=True)
 
     dfrestaurants = df.groupBy('`CUISINE DESCRIPTION`').count()
-    dfrestaurants = dfrestaurants.sort(['count'],ascending = False)
+    dfrestaurants = dfrestaurants.sort(['count'], ascending=False)
     dfrestaurants.show(85)
+
